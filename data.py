47419e7cadcc3a47db645f5b9db7fd682bf4e8f6
yoav1248 <yoav.alex1248@gmail.com>
2025-06-23 21:58:13 +0300

    Implemented data module

--- data.py
+++ data.py
@@ -1,7 +1,39 @@
 import numpy as np
 import pandas as pd
 from sklearn.model_selection import KFold
+
 np.random.seed(42)
+
+
+class StandardScaler:
+    def __init__(self):
+        """ object instantiation """
+        self.mean = None
+        self.std = None
+
+    def fit(self, X):
+        """ fit scaler by learning the mean and standard deviation per feature """
+        self.mean = X.mean()
+        self.std = X.std(ddof=1)
+
+    def transform(self, X):
+        """ transform X by learned mean and standard deviation, and return it """
+        return (X - self.mean()) / self.std()
+
+    def fit_transform(self, X):
+        """ fit scaler by learning the mean and standard deviation per feature, and then transform X """
+        self.fit(X)
+        return self.transform(X)
+
+
+def load_data(path):
+    """ reads and returns the pandas DataFrame """
+    return pd.read_csv(path)
+
+
+def adjust_labels(y):
+    """adjust labels of season from {0,1,2,3} to {0,1}"""
+    return y // 2
 
 
 def add_noise(data):
